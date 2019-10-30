69f7a730cf81080f9dabfc679bcdf60f94bf2d8e
Doctor Ned <1933864+Doctor-Ned@users.noreply.github.com>
2019-10-30 16:29:51 +0100

    Added static library generation for Spark and corrected ReflectionTest.

--- SparkTests/test.cpp
+++ SparkTests/test.cpp
@@ -2,14 +2,20 @@
 
 #include "Component.h"
 #include <rttr/registration>
+#include "Camera.h"
 
 TEST(ReflectionTest, ComponentReflectedProperly) {
 	rttr::type type{ rttr::type::get<spark::Component>() };
-	ASSERT_EQ(2, type.get_properties().size());
+	ASSERT_EQ(3, type.get_properties().size());
 	ASSERT_STREQ("Component", type.get_name().cbegin());
-	rttr::property activeProp {type.get_property("active")}, nameProp{ type.get_property("name") };
+	rttr::property activeProp {type.get_property("active")}, nameProp{ type.get_property("name") },
+		gameObjectProp{ type.get_property("gameObject") };
 	ASSERT_TRUE(activeProp.is_valid());
 	ASSERT_TRUE(nameProp.is_valid());
+	ASSERT_TRUE(gameObjectProp.is_valid());
 	ASSERT_STREQ("bool", activeProp.get_type().get_name().cbegin());
-	ASSERT_STREQ("std::string", nameProp.get_type().get_name().cbegin());
+}
+
+TEST(ReflectionTest, CameraPropertiesReflectedProperly) {
+	spark::Camera camera{};
 }
