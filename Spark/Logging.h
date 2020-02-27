7da72a219a7e6f505176e00d3ffd203e465f7ae5
Doctor Ned <1933864+Doctor-Ned@users.noreply.github.com>
2020-02-27 13:02:15 +0100

    Started working on smart pointer null value deserialization (partly working) and added more tests (failing right now because of improper raw pointer nullptr injection)

--- Spark/Logging.h
+++ Spark/Logging.h
@@ -16,7 +16,9 @@
 /// Defines active logging level of Spark
 /// //todo: might want to make it changeable
 ///
+#ifndef SPARK_ACTIVE_LEVEL
 #define SPARK_ACTIVE_LEVEL SPARK_LEVEL_DEBUG
+#endif
 
 #define SPARK_LOGGER_CALL(logger, level, ...) logger->log(spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, level, __VA_ARGS__)
 
